3ae3cc72493f2f6354d72db34ec1a13fae1f6b11
Jerry Greeenberg <jpg@sdsc.edu>
2012-04-13 19:14:04 +0000

    build texinfo before gdb

--- src/linux.mk
+++ src/linux.mk
@@ -60,5 +60,7 @@ SRCDIRS = `find . -maxdepth 1 -type d \
         -not -name gcc \
         -not -name mpc \
         -not -name mpfr \
-        -not -name .` mpfr mpc gcc
+        -not -name texinfo \
+        -not -name gdb \
+        -not -name .` mpfr mpc gcc texinfo gdb
 
